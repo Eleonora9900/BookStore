37d81634912fb67f803b8c19c6a1875bee2ef5e8
Cwetan <cvetan.atanasov@abv.bg>
2023-12-15 18:46:16 +0200

    Initial setup

---
+++ BookStore_BL/Services/IAuthorService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore_BL.Interfaces;
+using BookStore_DL.Interfaces;
+using BookStore_Models.Models;
+
+namespace BookStore_BL.Services
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorService(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public void Add(Author author)
+        {
+            _authorRepository.Add(author);
+        }
+
+        public void Delete(int id)
+        {
+            _authorRepository.Delete(id);
+        }
+
+        public List<Author> GetAll()
+        {
+            return _authorRepository.GetAll();  
+        }
+
+        public Author? GetById(int id)
+        {
+            return _authorRepository.GetById(id);
+        }
+    }
+}
---
+++ BookStore_DL/MemoryDB/InMemoryDB.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookStore_Models.Models;
+
+namespace BookStore_DL.MemoryDB
+{
+    public static class InMemoryDB
+    {
+        public static List<Author> AuthorData = new List<Author>()
+        {
+            new Author()
+            {
+                Id = 1,
+                Name = "Author 1",
+                BirthDay = DateTime.Now
+            },
+              new Author()
+            {
+                Id = 2,
+                Name = "Author 2",
+                BirthDay = DateTime.Now
+            },
+                new Author()
+            {
+                Id = 3,
+                Name = "Author 3",
+                BirthDay = DateTime.Now
+            }
+        };
+    }
+}
